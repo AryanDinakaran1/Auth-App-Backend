7178fc1e8086536b34649c018612addff632535b
Aryan Dinakaran <arydinakarangmail.com@Aryans-MacBook-Pro.local>
2022-10-18 00:09:11 +0530

    Did something cooler

--- main.py
+++ main.py
@@ -7,16 +7,27 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from models import Users, session
 
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*']
+    allow_origins=['http://localhost:3000']
 )
 
 @app.get('/')
 async def get_users():
-    pass
+    users = session.query(Users)
+    usersDict = {}
+
+    for user in users:
+        usersDict[int(user.id)] = {
+            'first_name' : user.first_name,
+            'last_name' : user.last_name,
+            'username' : user.username,
+        }
+
+    return usersDict
 
 @app.post('/signup')
 async def signup(username: str = Form(...), first_name: str = Form(...), last_name: str = Form(...), password: str = Form(...)):
