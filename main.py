ef7cc1ddf37f35812efae972f6e1cc13b5f5888e
Aryan Dinakaran <arydinakarangmail.com@Aryans-MacBook-Pro.local>
2022-10-17 14:04:58 +0530

    Sign Up Done

--- main.py
+++ main.py
@@ -5,15 +5,7 @@ from decouple import config
 from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
 
-con = mysql.connector.connect(
-    host = config('HOST'),
-    user = config('USER'),
-    port = config('PORT'),
-    password = config('PASSWORD'),
-    database = config('DATABASE')
-)
-
-mycursor = con.cursor()
+from models import Users, session
 
 app = FastAPI()
 
@@ -24,22 +16,17 @@ app.add_middleware(
 
 @app.get('/')
 async def get_users():
-    mycursor.execute('SELECT * FROM Users')
-    results = mycursor.fetchall()
-
-    print(results)
-
-    return {}
+    pass
 
 @app.post('/signup')
 async def signup(username: str = Form(...), first_name: str = Form(...), last_name: str = Form(...), password: str = Form(...)):
-    insertStatement = 'INSERT INTO Users (username, first_name, last_name, password) VALUES (%s,%s,%s,%s)'
+
+    
     password = hashlib.sha256(password.encode()).hexdigest()
-    val = (username, first_name, last_name, password)
 
-    mycursor.execute(insertStatement, val)
-
-    con.commit()
+    newUser = Users(username=username, first_name=first_name, last_name=last_name, password=password)
+    session.add(newUser)
+    session.commit()
 
     # Write code to redirect to an external page and test it
 
---
+++ models.py
@@ -0,0 +1,26 @@
+from urllib.parse import quote
+
+from sqlalchemy import create_engine, engine_from_config, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from decouple import config
+
+engine = create_engine(f"postgresql://postgres:%s@192.168.29.69:5432/AuthApp" % quote(config('PASSWORD')), echo=False)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+# User Table
+class Users(Base):
+    __tablename__ = "Users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(100))
+    first_name = Column(String(100))
+    last_name = Column(String(100))
+    password = Column(String(100))
+
+Base.metadata.create_all(engine)
